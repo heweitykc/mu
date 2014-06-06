4a9379e69d3161c6884a4609b12840888e5f4575
callee <heweitykc@163.com>
2014-06-06 17:48:58 +0800

    js

--- serverdemo/server.js
+++ serverdemo/server.js
@@ -1,39 +1,9 @@
 var net = require('net');
-var timeout = 20000;//超时
+var game = require('./game.js');
+
 var listenPort = 7003;//监听端口
 
-function clientIn(socket)
-{
-	// 我们获得一个连接 - 该连接自动关联一个socket对象
-    console.log('new client: ' +
-        socket.remoteAddress + ':' + socket.remotePort);
-    socket.setEncoding('binary');
-
-    //超时事件
-    socket.setTimeout(timeout,function(){
-        console.log('连接超时');
-        socket.end();
-    });
-
-    //接收到数据
-    socket.on('data',function(data){
-        console.log('recv:' + data);
-    });
-
-    //数据错误事件
-    socket.on('error',function(exception){
-        console.log('socket error:' + exception);
-        socket.end();
-    });
-    //客户端关闭事件
-    socket.on('close',function(data){
-        console.log('client close: ' +
-            socket.remoteAddress + ' ' + socket.remotePort);
-
-    });
-}
-
-var server = net.createServer(clientIn)
+var server = net.createServer(game.clientIn)
 server.listen(listenPort);
 
 //服务器监听事件
